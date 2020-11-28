17811e346ff346eb3926459608c57cd63121f674
Dan Vanderkam <danvdk@gmail.com>
2020-11-28 16:32:52 -0500

    Fix math error

--- src/perf_test.ts
+++ src/perf_test.ts
@@ -1,5 +1,6 @@
 import { Boggler, loadDictionary } from "./boggler";
-import { Trie } from "./trie";
+
+const TWO_32 = Math.pow(2, 32);
 
 (async () => {
   const dict = await loadDictionary('../performance-boggle/words');
@@ -7,14 +8,14 @@ import { Trie } from "./trie";
 
   let prime = (1 << 20) - 3;
   let totalScore = 0;
-  let hash = new Uint32Array(1);
+  let hash = 0;
   let numBoards = 0;
   const bases = ["abcdefghijklmnop", "catdlinemaropets"];
 
   const startMs = Date.now();
 
   for (let _rep = 0; _rep < 10; _rep++) {
-    hash[0] = 1234;
+    hash = 1234;
     for (const base of bases) {
       boggler.parseBoard(base);
       for (let y1 = 0; y1 < 4; y1++) {
@@ -24,9 +25,9 @@ import { Trie } from "./trie";
             for (let c2 = 0; c2 < 26; c2++) {
               boggler.setCell(2, y2, c2);
               const score = boggler.score();
-              hash[0] *= 123;
-              hash[0] += score;
-              hash[0] = hash[0] % prime;
+              hash *= 123 + score;
+              hash = (hash % prime);
+              hash = hash % TWO_32;
               totalScore += score;
               numBoards++;
             }
@@ -39,7 +40,7 @@ import { Trie } from "./trie";
   const elapsedMs = Date.now() - startMs;
 
   console.log('Total score:', totalScore, '=', totalScore/numBoards, 'pts/bd');
-  console.log('Score hash:', hash[0].toString(16));
+  console.log('Score hash:', hash.toString(16));
   console.log('Evaluated', numBoards, 'in', (elapsedMs / 1000), 'seconds =', numBoards / elapsedMs * 1000, 'bds/sec');
 })().catch(e => {
   console.error(e);
